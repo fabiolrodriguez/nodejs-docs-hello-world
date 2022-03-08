9d4db07490c52b1ac67b5433d49065d76abc792e
fabiolrodriguez <fabio.rodriguez@gmail.com>
2022-03-08 17:23:17 -0300

    print env var

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const http = require('http');
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Testing Action v2");
+    response.end(process.env.DB_URL);
 });
 
 const port = process.env.PORT || 1337;
