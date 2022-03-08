a264916829a2570a528fbc7e8bb8a2c58f364dd4
fabiolrodriguez <fabio.rodriguez@gmail.com>
2022-03-08 19:44:19 -0300

    send server type

--- index.js
+++ index.js
@@ -1,88 +1,61 @@
-// const http = require('http');
+const http = require('http');
 
-// const server = http.createServer((request, response) => {
-//     response.writeHead(200, {"Content-Type": "text/plain"});
-//     response.end("Testing connection");
-// });
-
-// const port = process.env.PORT || 1337;
-// server.listen(port);
-
-// console.log("Server running at http://localhost:%d", port);
-
-
-// var express = require('express');
-// var app = express();
-
-// app.get('*',function(req,res){
-
-//   res.send("OK");
-
-// });
-
-
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
-
+const server = http.createServer((request, response) => {
+    response.writeHead(200, {"Content-Type": "text/plain"});
     // Import the mssql package
 
-    // var sql = require("mssql");
+    var sql = require("mssql");
 
-    // // Create a configuration object for our Azure SQL connection parameters
-    // var dbConfig = {
-    // server: "DB_URL", // Use your SQL server name
-    // database: "DB_NAME", // Database to connect to
-    // user: "DB_USER", // Use your username
-    // password: "DB_PASS", // Use your password
-    // port: 1433,
-    // // Since we're on Windows Azure, we need to set the following options
-    // options: {
-    //       encrypt: true
-    //   }
-    // };
+    // Create a configuration object for our Azure SQL connection parameters
+    var dbConfig = {
+    server: "DB_URL", // Use your SQL server name
+    database: "DB_NAME", // Database to connect to
+    user: "DB_USER", // Use your username
+    password: "DB_PASS", // Use your password
+    port: 1433,
+    // Since we're on Windows Azure, we need to set the following options
+    options: {
+          encrypt: true
+      }
+    };
 
-    // // This function connects to a SQL server, executes a SELECT statement,
-    // // and displays the results in the console.
-    // function getCustomers() {
-    // // Create connection instance
-    // var conn = new sql.Connection(dbConfig);
+    // This function connects to a SQL server, executes a SELECT statement,
+    // and displays the results in the console.
+    function getCustomers() {
+    // Create connection instance
+    var conn = new sql.Connection(dbConfig);
 
-    // conn.connect()
-    // // Successfull connection
-    // .then(function () {
+    conn.connect()
+    // Successfull connection
+    .then(function () {
 
-    //   // Create request instance, passing in connection instance
-    //   var req = new sql.Request(conn);
+      // Create request instance, passing in connection instance
+      var req = new sql.Request(conn);
 
-    //   // Call mssql's query method passing in params
-    //   req.query("SELECT * FROM customers")
-    //   .then(function (recordset) {
-    //     res.send(recordset);
-    //     conn.close();
-    //   })
-    //   // Handle sql statement execution errors
-    //   .catch(function (err) {
-    //     res.send(err);
-    //     conn.close();
-    //   })
+      // Call mssql's query method passing in params
+      req.query("SELECT * FROM customers")
+      .then(function (recordset) {
+        response.send(recordset);
+        conn.close();
+      })
+      // Handle sql statement execution errors
+      .catch(function (err) {
+        response.send(err);
+        conn.close();
+      })
 
-    // })
-    // // Handle connection errors
-    // .catch(function (err) {
-    //   console.log(err);
-    //   conn.close();
-    // });
-    // }
+    })
+    // Handle connection errors
+    .catch(function (err) {
+      response.send(err);
+      conn.close();
+    });
+    }
 
-    // getCustomers();
+    getCustomers();
+});
 
+const port = process.env.PORT || 1337;
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
