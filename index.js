da6bf35d75321585115b132a44fa198fb4498f23
fabiolrodriguez <fabio.rodriguez@gmail.com>
2022-03-08 17:26:17 -0300

    change response

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Testing Action v2");
+    // response.end("Testing Action v2");
     response.end(process.env.DB_URL);
 });
 
