594dad5f53a12681db53b87733a58b769e1109ba
fabiolrodriguez <fabio.rodriguez@gmail.com>
2022-03-08 17:43:18 -0300

    change console log

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const port = process.env.PORT || 1337;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
+console.log(process.env);
+
 
 
 // const { Connection, Request } = require("tedious");
